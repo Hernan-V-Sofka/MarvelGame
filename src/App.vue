f30fe9d4349729d3ff1521ec1346bb60b14f75c0
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-03-15 11:09:15 -0500

    Redirecionamiento del formulario de login para al campo de juego, ademas de las convinaciones del componente de las cartas, junto al componente del campo.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,18 @@
 <template>
-  <!-- <div class="d-grid gap-2 max-auto">
-    <StartGame msg="Start Game"/>
-  </div> -->
-  <!-- <div class="text-center">
-    <CampoGame />
-  </div> -->
-  <div class="text-center">
-    <Carts />
+  <div class="text-cener">
+    <StartGame msg="Marvel Game"/>
   </div>
 </template>
 
-
-
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue';
-// import StartGame from '@/components/StartGame.vue';
-// import CampoGame from '@/components/CampoGame.vue';
-import Carts from '@/components/CartsGame.vue';
+import { defineComponent } from 'vue';
+import StartGame from '@/components/StartGame.vue';
 
 export default defineComponent({
   name: 'App',
-  components:{Carts},
-  setup() {
-      return { 
-
-      }
-  },
-})
+  components:{ 
+      StartGame, 
+  }
+});
 </script>
 
--- src/components/StartGame.vue
+++ src/components/StartGame.vue
@@ -1,55 +1,69 @@
 <template>
-    <section class="vh-100" style="background-color: #F0131E;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                        <div class="card-body p-5 text-center">
-
-                            <h3 class="mb-5">{{ msg }}</h3>
+<section class="vh-100" style="background-color: #F0131E;" v-if="oculeteLogin">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <h3 class="mb-5">{{ msg }}</h3>
                             
-                            <div class="form-outline mb-4">
-                                <label class="form-label" for="typeEmailX-2">Email</label>
-                                <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                            </div>
-
-                            <div class="form-outline mb-4">
-                                <label class="form-label" for="typePasswordX-2">Password</label>
-                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                            </div>
-
-                            <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
-
-                            <hr class="my-4">
-
-                            <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #dd4b39;" type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
-                            <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;" type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
+                        <div class="form-outline mb-4">
+                            <label class="form-label" for="typeEmailX-2">Email</label>
+                            <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
                         </div>
+
+                        <div class="form-outline mb-4">
+                            <label class="form-label" for="typePasswordX-2">Password</label>
+                            <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
+                        </div>
+
+                        <button class="btn btn-primary btn-lg btn-block" type="submit" @click="changeVicible">Login</button>
+
+                        <hr class="my-4">
+
+                        <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #dd4b39;" type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
+                        <button class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;" type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
+
+
+<div class="text-center" >
+    <CampoGame v-if="isVicible" />
+    <Carts />
+</div>
+
 </template>
 
 <script lang= "ts">
-import { defineComponent, reactive } from 'vue';
+import { defineComponent } from 'vue';
+import CampoGame from '@/components/CampoGame.vue';
+import Carts from '@/components/CartsGame.vue';
 
 export default defineComponent({   
    name: 'StartGame',
+   components: {
+    CampoGame,
+    Carts
+   },
    props: {
        msg: String,
    },
-   setup(){
-    //    const form = reactive({
-    //        userName: '',
-    //        password: ''
-    //    });
-
-    //    const onSubmit = () => {}
-
-    //    return { form, onSubmit }
-   }
+   data(){
+       return { 
+        isVicible: false,
+        oculeteLogin: true
+       }
+    },
+   methods:{
+    changeVicible(){
+      this.isVicible = true;
+      this.oculeteLogin = false;
+    }
+  }
 });
 </script>
 
