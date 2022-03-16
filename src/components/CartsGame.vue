7eb534e615af2064c0266ac8df375724a0ba3f14
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-03-16 11:00:34 -0500

    Configuracion del estilo de las tarjetas y sus respectivos cambios centrados en las vistas.

--- src/components/CartsGame.vue
+++ src/components/CartsGame.vue
@@ -1,64 +1,128 @@
 <template>
-<div class="card-group" >
-  <div class="card">
-    <img :src="imgUrl" class="card-img-top" alt="Carts">
-    <div class="card-body">
-      <h5 class="card-title">Doctor Dom</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<div class="card-grup">
+  <div class="row d-flex justify-content-center">
+    <div class="col-md-5 col-lg-2">
+      <div class="card">
+        <img :src="imgUrl" class="card-img-top" alt="ImgCarts" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
     </div>
-    <div class="card-footer">
-      <small class="text-muted">Power: 3000s</small>
-    </div>
-  </div>
 
-  <div class="card">
-    <img :src="imgUrl" class="card-img-top" alt="Carts">
-    <div class="card-body">
-      <h5 class="card-title">Doctor Dom</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <div class="col-md-5 col-lg-2">
+      <div class="card">
+        <img :src="imgUrl" class="card-img-top" alt="ImgCarts" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
     </div>
-    <div class="card-footer">
-      <small class="text-muted">Power: 3000s</small>
+
+
+    <div class="col-md-5 col-lg-2">
+      <div class="card">
+        <img :src="imgUrl" class="card-img-top" alt="ImgCarts" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col-md-5 col-lg-2">
+      <div class="card">
+        <img :src="imgUrl" class="card-img-top" alt="ImgCarts" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="col-md-5 col-lg-2">
+      <div class="card">
+        <img :src="imgUrl" class="card-img-top" alt="ImgCarts" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="card">
-    <img :src="imgUrl" class="card-img-top" alt="Carts">
-    <div class="card-body">
-      <h5 class="card-title">Doctor Dom</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Power: 3000s</small>
-    </div>
   </div>
-</div>
 </template>
 
+<style scoped>
+.card-img-top {
+  width: 100%;
+  height: 12vw;
+  object-fit: cover;
+}
+</style>
+
 <script lang= "ts">
-import { defineComponent } from 'vue';
-import { collection, getDocs } from 'firebase/firestore/lite';
-import { db } from '@/main';
+import { defineComponent } from "vue";
+import { collection, getDocs } from "firebase/firestore/lite";
+import { db } from "@/main";
 
 export default defineComponent({
-    name: 'CartGame',
-    data(){ 
-      return {
-        imgUrl:''
+  name: "CartGame",
+  data() {
+    return {
+      imgUrl: "",
+    };
+  },
+  mounted() {
+    this.getDats();
+  },
+  methods: {
+    async getDats() {
+      try {
+        const coll = collection(db, "img");
+        const urlItem = await (
+          await getDocs(coll)
+        ).docs.map((docs) => docs.data().url);
+        this.imgUrl = urlItem[0];
+      } catch (error) {
+        console.log("Error");
       }
     },
-    mounted(){
-      this.getDats();
-    },  
-    methods:{
-      async getDats(){
-        try {
-          const coll = collection(db,'img');
-          const urlItem = await (await getDocs(coll)).docs.map(docs => docs.data().url);
-          this.imgUrl = urlItem[0];    
-        } catch (error) {
-          console.log('Error'); 
-        }
-      }
-    }
+  },
 });
 </script>
