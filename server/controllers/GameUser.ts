11a5478dc4be165b57f6a13b2b9c1e8b9241617b
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-03-23 22:38:22 -0500

    Se logro traer de manera dinamica, las cartas del servidor y guardar solo 5 cartas, permitiendo almacenarlas y poder enviarlas al fron-end, desde el punto de inicio del juego

--- server/controllers/GameUser.ts
+++ server/controllers/GameUser.ts
@@ -3,12 +3,21 @@ import RandomCarts from './carts/RandomCarts';
 import DB from '../firebase/Firebase';
 
 export const getCarts = async (req: Request, res: Response) => {
-    const carts = new RandomCarts();
-    let number = carts.genericNumberRamdom();
-    const querySnapshot = await (await DB.collection('Cartas').where("idCart" , "==", number[1]).get()).docs
-    
-    console.log(number);
-    
-    res.send(querySnapshot);
-    
+    try {
+        const CartInfo: Array<Object> = [];
+        const carts = new RandomCarts();
+
+        let number = carts.genericNumberRamdom();
+        const querySnapshot = await DB.collection('Cartas').get()
+        const item = querySnapshot.docs.map(doc => doc.data());
+
+        for (let i = 0; i <= number.length; i++) {
+            CartInfo.push(item[number[i]]);
+        }
+
+        res.send(CartInfo);
+    } catch (error) {
+
+    }
+
 }
--- server/controllers/carts/RandomCarts.ts
+++ server/controllers/carts/RandomCarts.ts
@@ -1,11 +1,10 @@
 class RandomCarts {
     private listNumber: Array<number> = [];
 
-
     public genericNumberRamdom(): Array<number> {
         let random: Array<number> = [];
         for (let i = 0; i < 5; i++) {
-            random[i] = Math.floor(Math.random() * (5 - 1) + 1);
+            random[i] = Math.floor(Math.random() * (10 - 1) + 1);
             this.listNumber.push(random[i]);
         }
         return this.listNumber;
